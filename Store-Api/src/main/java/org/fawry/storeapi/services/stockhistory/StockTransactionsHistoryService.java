91ac31d00b7b2e146ac404c3fc6c57000d82e1ac
Mohamed Shehab <mohamedshepo280@gmail.com>
2025-03-22 13:22:03 +0200

    Implementing logTransaction function

--- Store-Api/src/main/java/org/fawry/storeapi/services/stockhistory/StockTransactionsHistoryService.java
+++ Store-Api/src/main/java/org/fawry/storeapi/services/stockhistory/StockTransactionsHistoryService.java
@@ -1,4 +1,7 @@
 package org.fawry.storeapi.services.stockhistory;
 
+import org.fawry.storeapi.entities.TransactionType;
+
 public interface StockTransactionsHistoryService {
+    void logTransaction(Long stockId, int oldQuantity, int newQuantity, TransactionType transactionType);
 }
--- Store-Api/src/main/java/org/fawry/storeapi/services/stockhistory/StockTransactionsHistoryServiceImpl.java
+++ Store-Api/src/main/java/org/fawry/storeapi/services/stockhistory/StockTransactionsHistoryServiceImpl.java
@@ -1,7 +1,25 @@
 package org.fawry.storeapi.services.stockhistory;
 
+import org.fawry.storeapi.entities.Stock;
+import org.fawry.storeapi.entities.StockTransactionsHistory;
+import org.fawry.storeapi.entities.TransactionType;
+import org.fawry.storeapi.repositories.StockRepository;
+import org.fawry.storeapi.repositories.StockTransactionsHistoryRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StockTransactionsHistoryServiceImpl implements StockTransactionsHistoryService {
+    StockTransactionsHistoryRepository stockTransactionsHistoryRepository;
+    StockRepository stockRepository;
+
+    @Override
+    public void logTransaction(Long stockId, int oldQuantity, int newQuantity, TransactionType transactionType) {
+        StockTransactionsHistory stockTransactionsHistory = new StockTransactionsHistory();
+        Stock stock = stockRepository.findById(stockId).get();
+        stockTransactionsHistory.setStock(stock);
+        stockTransactionsHistory.setOldQuantity(oldQuantity);
+        stockTransactionsHistory.setNewQuantity(newQuantity);
+        stockTransactionsHistory.setTransactionType(transactionType);
+        stockTransactionsHistoryRepository.save(stockTransactionsHistory);
+    }
 }

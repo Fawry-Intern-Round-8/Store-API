338907b91065830151e6be9b6b4a4b811336296d
Loay Ghreeb <Loayahmed655@gmail.com>
2025-04-16 20:28:29 +0200

    Register services as Eureka clients

--- src/main/java/org/fawry/store/StoreApiApplication.java
+++ src/main/java/org/fawry/store/StoreApiApplication.java
@@ -2,12 +2,12 @@ package org.fawry.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication(scanBasePackages = "org.fawry.store")
+@EnableDiscoveryClient
 public class StoreApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(StoreApiApplication.class, args);
 	}
-
 }
